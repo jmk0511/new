681d4ab2c8e33f60ff782edbaf8e8db9d3202bac
jmk0511 <135510686+jmk0511@users.noreply.github.com>
2023-06-05 09:58:06 +0800

    new

--- walle.py
+++ walle.py
@@ -1,24 +1,26 @@
-
-
 import streamlit as st
 import os
 from fastai.vision.all import *
 
-import pathlib
-temp=pathlib.PosixPath
-pathlib.PosixPath=pathlib.WindowsPath
+# 获取当前文件所在的文件夹路径
+path = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(path, "export.pkl")
 
-path=os.path.dirname(os.path.abspath(__file__))
-model_path=os.path.join(path,'export.pkl')
+# Load the model
+learn_inf = load_learner(model_path)
 
-learn_inf=load_learner(model_path)
+st.title("Image Classification App")
+st.write("Upload an image and the app will predict the corresponding label.")
 
-pathlib.PosixPath=temp
+# Allow the user to upload an image
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-uploaded_file=st.file_uploader("Choose an image...",type=["jpg","jpeg","png"])
-
+# If the user has uploaded an image
 if uploaded_file is not None:
-    img=PILImage.create(uploaded_file)
-    st.image(img.to_thumb(500,500),caption='Your Image')
-    pred,pred_idx,probs=learn_inf.predict(img)
-    st.write(f'Prediction：{pred};Probability:{probs[pred_idx]:.04f}')
+    # Display the image
+    image = PILImage.create(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    # Get the predicted label
+    pred, pred_idx, probs = learn_inf.predict(image)
+    st.write(f"Prediction: {pred}; Probability: {probs[pred_idx]:.04f}")
